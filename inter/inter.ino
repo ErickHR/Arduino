ab5809a324585c4da2094b0d8c867adb072501f4
ErickHR <Erick08_999@hotmail.com>
2019-11-17 00:33:39 -0600

    init

---
+++ inter/inter.ino
@@ -0,0 +1,43 @@
+/*void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("www.TheEngineeringProjects.com");
+}
+
+void loop() 
+{
+    if(Serial.available())
+    {
+        char data = Serial.read();
+        Serial.print(data);
+    }
+}*/
+
+
+#include <SoftwareSerial.h> 
+
+SoftwareSerial SoftSerial(2, 3);
+
+void setup()
+{
+  Serial.begin(9600);
+  SoftSerial.begin(9600);
+  
+  SoftSerial.println("       **** Its a Software Serial **** ");
+  SoftSerial.println(" Designed by www.TheEngineeringProjects.com");
+  SoftSerial.println();
+  
+  Serial.println("       **** Its a Hardware Serial **** ");
+  Serial.println(" Designed by www.TheEngineeringProjects.com");
+  Serial.println();
+}
+
+void loop()
+{
+      
+      if (Serial.available())
+      {
+           char data = Serial.read();
+           SoftSerial.print(data);
+      }
+}
